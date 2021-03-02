a8fca346f5b93aec6c745b4c6cdc9994c986c0ca
leylee <leylee2001@gmail.com>
2021-03-02 18:26:52 +0800

    Add pwm_driver

---
+++ Inc/pwm_driver.h
@@ -0,0 +1,17 @@
+#ifndef __PWM_DRIVER_H_
+#define __PWM_DRIVER_H_
+
+#include "pca9685.h"
+
+#define PWM_MAX_OFF_TIME 4096
+
+typedef struct {
+  pca9685_handle_t *hpca;
+  uint8_t channel;
+} pwm_handle_t;
+
+bool pwm_init(pca9685_handle_t *hpca);
+bool pwm_set_duty_cycle(pwm_handle_t *hpwm, float duty_cycle);
+bool pwm_set_off_time(pwm_handle_t *hpwm, uint8_t off_time);
+
+#endif  // !__PWM_DRIVER_H_
---
+++ Src/pwm_driver.c
@@ -0,0 +1,50 @@
+#include "pwm_driver.h"
+
+/**
+ * @brief 初始化 PWM 驱动板
+ *
+ * @param hpca PCA9685 驱动 handle
+ * @return true 初始化成功
+ * @return false 初始化失败
+ */
+bool pwm_init(pca9685_handle_t *hpca)
+{
+  if (pca9685_init(hpca) && pca9685_set_pwm_frequency(hpca, 50.0))
+    return true;
+  else
+    return false;
+}
+
+/**
+ * @brief 设置一个 PWM 通道的占空比
+ *
+ * @param hpwm PWM 通道 handle
+ * @param duty_cycle 占空比, 在 [0.0, 1.0] 之间
+ * @return true 设置成功
+ * @return false 设置失败
+ */
+bool pwm_set_duty_cycle(pwm_handle_t *hpwm, float duty_cycle)
+{
+  if (duty_cycle >= 0.0 && duty_cycle <= 1.0)
+    return pca9685_set_channel_duty_cycle(hpwm->hpca, hpwm->channel, duty_cycle,
+                                          false);
+  else
+    return false;
+}
+
+/**
+ * @brief 设置一个 PWM 通道的高电平时长.
+ *
+ * @param hpwm PWM 通道 handle
+ * @param off_time 高电平时长, 在 [0, PWM_MAX_OFF_TIME] 之间
+ * @return true 设置成功
+ * @return false 设置失败
+ */
+bool pwm_set_off_time(pwm_handle_t *hpwm, uint8_t off_time)
+{
+  if (off_time >= 0 && off_time <= PWM_MAX_OFF_TIME)
+    return pca9685_set_channel_pwm_times(hpwm->hpca, hpwm->channel, 0,
+                                         off_time);
+  else
+    return false;
+}
