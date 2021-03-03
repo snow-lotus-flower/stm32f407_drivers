ccccfa81f1aa17fb2edba28b9123179967a1a376
leylee <leylee2001@gmail.com>
2021-03-04 01:59:46 +0800

    optimize header file definition

--- Inc/pwm_driver.h
+++ Inc/pwm_driver.h
@@ -1,5 +1,5 @@
-#ifndef __PWM_DRIVER_H_
-#define __PWM_DRIVER_H_
+#ifndef __PWM_DRIVER_H__
+#define __PWM_DRIVER_H__
 
 #include "pca9685.h"
 
@@ -14,4 +14,4 @@ bool pwm_init(pca9685_handle_t *hpca);
 bool pwm_set_duty_cycle(pwm_handle_t *hpwm, float duty_cycle);
 bool pwm_set_off_time(pwm_handle_t *hpwm, uint8_t off_time);
 
-#endif  // !__PWM_DRIVER_H_
+#endif  // !__PWM_DRIVER_H__
