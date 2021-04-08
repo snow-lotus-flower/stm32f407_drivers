5e3831228b0b50aed5c3de949de82840a9f19bd8
leylee <leylee2001@gmail.com>
2021-04-08 19:46:36 +0800

    add comment to pid.c

--- Src/pid.c
+++ Src/pid.c
@@ -4,8 +4,7 @@
 #include "tim.h"
 
 /** 轮速闭环 PID */
-void PID_wheel_init(PIDWheel_HandleTypeDef *pid)
-{
+void PID_wheel_init(PIDWheel_HandleTypeDef *pid) {
   pid->SetSpeed = 0.0;
   pid->AltualSpeed = 0.0;
   pid->error = 0.0;
@@ -22,13 +21,23 @@ void PID_wheel_init(PIDWheel_HandleTypeDef *pid)
   pid->imin = -0.2;
 }
 
-void PID_wheel_realize(PIDWheel_HandleTypeDef *pid)
-{
+/**
+ * @brief 轮速闭环 PID 计算
+ *
+ * @param pid PIDWheel handle
+ */
+void PID_wheel_realize(PIDWheel_HandleTypeDef *pid) {
   int index;
+  // 计算误差
   pid->error = pid->SetSpeed - pid->AltualSpeed;
 
+  /* 抗积分饱和
+    1. 判断输出值是否达到极值. 若是, 则不再增加此方向的积分;
+    2. 判断误差是否大于积分开始的阈值. 若当前误差很大, 则没有必要进行积分;
+    3. 判断积分项的方向是否和误差项相反. 只进行缩小误差方向上的积分.
+  */
   if (pid->voltage >= pid->umax) {
-    if (fabs(pid->error) > pid->erromax)  //�ǵ���ǰ����� #include "stdlib.h"
+    if (fabs(pid->error) > pid->erromax)  //
     {
       index = 0;
     } else {
@@ -55,19 +64,27 @@ void PID_wheel_realize(PIDWheel_HandleTypeDef *pid)
       pid->integral += pid->error;
     }
   }
+
+  // 计算加比例后的积分项
   float integral = pid->Ki * pid->integral;
+  // 如果想让轮子停下, 且实际速度已经为0, 则对积分项清零
   if (pid->SetSpeed == 0.0 && pid->AltualSpeed == 0) pid->integral = 0;
+  // 如果积分项很大, 超过了规定的阈值, 则限制在阈值内
   if (integral > pid->imax) pid->integral = pid->imax / pid->Ki;
   if (integral < pid->imin) pid->integral = pid->imin / pid->Ki;
+
+  // 计算占空比 (执行器)
   pid->voltage = pid->Kp * pid->error + index * pid->Ki * pid->integral +
                  pid->Kd * (pid->error - pid->erro_last);
+
+  // 限制执行器的输出阈值
   if (pid->voltage > 0.6) pid->voltage = 0.6;
   if (pid->voltage < -0.6) pid->voltage = -0.6;
+
   pid->erro_last = pid->error;
 }
 
-void PID_yaw_init(PIDYaw_HandleTypeDef *pid)
-{
+void PID_yaw_init(PIDYaw_HandleTypeDef *pid) {
   pid->SetDeg = 0.0;
   pid->AltualDeg = 0.0;
   pid->error = 0.0;
@@ -84,8 +101,7 @@ void PID_yaw_init(PIDYaw_HandleTypeDef *pid)
   pid->imin = -0.1;
 }
 
-void PID_yaw_realize(PIDYaw_HandleTypeDef *pid)
-{
+void PID_yaw_realize(PIDYaw_HandleTypeDef *pid) {
   int index;
   pid->error = pid->SetDeg - pid->AltualDeg;
 
